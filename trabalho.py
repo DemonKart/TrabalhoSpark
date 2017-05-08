3508ce9b3648282ed37416f0e3450cd6a90ba957
alangit <apcsilva@inf.ufpr.br>
2017-05-08 19:57:59 -0300

    Add Machine Learning simple decision tree alg

--- trabalho.py
+++ trabalho.py
@@ -43,10 +43,10 @@ courses = spark.read.format("com.databricks.spark.csv").option("header",'true').
 institutions = spark.read.format("com.databricks.spark.csv").option("header",'true').option("delimiter", '|').load("dados/institution_no_header.csv", inferSchema=True)
 
 students = students.select("CO_CURSO", "CO_IES", "IN_SEXO_ALUNO", "NU_IDADE_ALUNO", "CO_COR_RACA_ALUNO", "CO_TIPO_ESCOLA_ENS_MEDIO")
-courses = courses.select("CO_CURSO", "NO_CURSO")
+courses = courses.select("CO_CURSO", "NO_OCDE")
 institutions = institutions.select("CO_IES", "CO_UF_IES")
 
-course_names = courses.select("NO_CURSO").distinct().collect()
+course_names = courses.select("NO_OCDE").distinct().collect()
 
 print "Resulting table created\n---\n"
 
@@ -59,9 +59,9 @@ for course_name in course_names:
 courses2 = courses.collect()
 courses = []
 for course in courses2:
-	courses.append((course[0],names.index(Row(NO_CURSO=course[1]))))
+	courses.append((course[0],names.index(Row(NO_OCDE=course[1]))))
 
-courses = spark.createDataFrame(courses, ["CO_CURSO", "NO_CURSO"])
+courses = spark.createDataFrame(courses, ["CO_CURSO", "NO_OCDE"])
 
 print "Applying ML\n"
 
@@ -69,28 +69,32 @@ sc = students.join(courses, "CO_CURSO")
 sci = sc.join(institutions, "CO_IES")
 
 # CO_UF_IES
-# NO_CURSO
+# NO_OCDE
 # IN_SEXO_ALUNO
 # NU_IDADE_ALUNO
 # CO_COR_RACA_ALUNO
 # CO_TIPO_ESCOLA_ENS_MEDIO
+todas = ['NU_IDADE_ALUNO', 'IN_SEXO_ALUNO', 'CO_TIPO_ESCOLA_ENS_MEDIO', 'CO_COR_RACA_ALUNO', 'CO_UF_IES', 'NO_OCDE']
 
-# features = ['type', 'responsable', 'stages']
-# assembler = VectorAssembler(inputCols=features, outputCol="features")
-# dataFinal = assembler.transform(completeData)
-
-# dt = DecisionTreeClassifier(labelCol='uf_code', featuresCol='features', maxDepth=5)
-# (treinamento, teste) = dataFinal.randomSplit([0.8, 0.2])
-# model = dt.fit(treinamento)
-# predictions = model.transform(teste)
-# print model.toDebugString
-# total = predictions.count()
-# missed = predictions.where("uf_code != prediction").count()
+for i in range(0,len(todas)-1):
+	features = todas
+	print i
+	varx = features.pop(0)
+	assembler = VectorAssembler(inputCols=features, outputCol="features")
+	dataFinal = assembler.transform(sci)
+	dt = DecisionTreeClassifier(labelCol=varx, featuresCol='features', maxDepth=5)
+	(treinamento, teste) = dataFinal.randomSplit([0.8, 0.2])
+	model = dt.fit(treinamento)
+	predictions = model.transform(teste)
+	# print model.toDebugString
+	total = predictions.count()
+	missed = predictions.where(str(varx)+" != prediction").count()
+	print varx + ": ", total, missed, float(missed) / float(total)
 
 # resultados.append({"id": "Unidade Federativa", "TOTAL": total, "MISSED": missed})
 
 # SELECT  i.CO_UF_IES, 
-#         c.NO_CURSO,
+#         c.NO_OCDE,
 #         s.IN_SEXO_ALUNO,
 #         s.NU_IDADE_ALUNO,
 #         s.CO_COR_RACA_ALUNO,
@@ -104,4 +108,4 @@ sci = sc.join(institutions, "CO_IES")
 # IES   estado                                          CO_UF_IES
 # ALUNO escola publica x privada        CO_TIPO_ESCOLA_ENS_MEDIO
 # ALUNO raça                                            CO_COR_RACA_ALUNO
-# CURSO nome                                            NO_CURSO
+# CURSO nome                                            NO_OCDE
